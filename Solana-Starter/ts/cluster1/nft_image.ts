b2876102e7b38b8470836954416415d2b9e25030
tusharpamnani <tusharpamnani1@gmail.com>
2025-07-03 14:20:02 +0000

    rug day

---
+++ Solana-Starter/ts/cluster1/nft_image.ts
@@ -0,0 +1,32 @@
+import wallet from "../Turbin3-wallet.json"
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import { readFile } from "fs/promises"
+
+// Create a devnet connection
+const umi = createUmi('https://api.devnet.solana.com');
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(irysUploader());
+umi.use(signerIdentity(signer));
+
+(async () => {
+    try {
+        //1. Load image
+        //2. Convert image to generic file.
+        //3. Upload image
+
+        const imagePath = await readFile("/home/tushar/Q3_2025_Builder_TusharPamnani/Solana-Starter/ts/cluster1/jeff.png")
+
+        const image = createGenericFile(imagePath, "imagee.png", {contentType: "image/png"})
+
+        const [myUri] = await umi.uploader.upload([image]);
+        console.log("Your image URI: ", myUri);
+    }
+    catch(error) {
+        console.log("Oops.. Something went wrong", error);
+    }
+})();
